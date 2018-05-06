0fc86e5c8c7054534e12cd7f0997c145588d7c08
mgmilton <matt.milton@gmail.com>
2018-05-05 18:45:14 -0600

    adds update and delete methods

--- models/meal.js
+++ models/meal.js
@@ -27,6 +27,17 @@ class Meal {
       .leftJoin('foods', 'meal_foods.food_id','foods.id')
       .groupBy('meals.id')
   }
+
+  static create(meal_id, food_id){
+    return database('meal_foods').insert([{meal_id: meal_id, food_id: food_id}])
+  }
+
+  static destroy(meal_id, food_id){
+    return database('meal_foods')
+      .where('meal_foods.meal_id', meal_id)
+      .where('meal_foods.food_id', food_id)
+      .del()
+  }
 }
 
 module.exports = Meal
