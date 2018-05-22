95a72da03c6bf7c7a20ff69b8be97ea240d6c852
mgmilton <matt.milton@gmail.com>
2018-05-22 11:58:31 -0600

    adds mealsWhenEaten attribute to foods

---
+++ db/migrations/20180522114959_update_foods_withmealseaten.js
@@ -0,0 +1,10 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.alterTable('foods', function(table){
+    table.specificType('mealsWhenEaten', 'INT[]');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('foods')
+};
