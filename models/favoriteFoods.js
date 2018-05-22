f890e8e8357e764a43cea59d0b1249c1a51e73f0
mgmilton <matt.milton@gmail.com>
2018-05-22 11:26:31 -0600

    updates all method

--- models/favoriteFoods.js
+++ models/favoriteFoods.js
@@ -2,8 +2,22 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
+const foodsAsJson = () => (
+  database.raw(`ARRAY_TO_JSON(
+    ARRAY_REMOVE(ARRAY_AGG(foods.*), NULL)
+  ) AS foods`)
+)
+
+
 class FavoriteFoods {
-  static all() {
-    return database('favoritefoods').select('*')
+  static all(){
+    return database('favoritefoods')
+    .select(['favoritefoods.timesEaten', foodsAsJson()])
+    .leftJoin('foods_favfoods', 'favoritefoods.id','foods_favfoods.favoritefood_id')
+    .leftJoin('foods', 'foods_favfoods.food_id','foods.id')
+    .groupBy('favoritefoods.timesEaten')
+    .orderBy('favoritefoods.timesEaten')
   }
 }
+
+module.exports = FavoriteFoods
