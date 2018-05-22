f0a9a65a2db400b63c63b38e2008d428ffacd147
mgmilton <matt.milton@gmail.com>
2018-05-22 11:25:20 -0600

    renamed file

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var foodsRouter = require('./routes/api/v1/foods')
 var mealsRouter = require('./routes/api/v1/meals')
+var favfoodsRouter = require('./routes/api/v1/favoritefoods')
 var app = express();
 
 
@@ -32,6 +33,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/v1/foods', foodsRouter);
 app.use('/api/v1/meals', mealsRouter);
+app.use('/api/v1/favorite_foods', favfoodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
